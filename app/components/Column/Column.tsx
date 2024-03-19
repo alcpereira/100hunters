a474b97839356ab6267fb2fd04365b8ad0359ab3
Charles Errington <charleserringtondev@gmail.com>
2024-03-19 20:34:55 +0100

    chore: remove test id

--- app/components/Column/Column.tsx
+++ app/components/Column/Column.tsx
@@ -6,7 +6,7 @@ export default function Column() {
       className="border-1 rounded-md border border-solid border-gray-700 px-2 py-5"
       onDragOver={() => console.log("dragging over")}
     >
-      <h1 data-testid="column-title">This is a column</h1>
+      <h1>This is a column</h1>
       <Card title="hello" content="world" />
     </div>
   );
